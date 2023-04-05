484e6f68569cbfce9e1b62d8126eec0de5cf9b6a
poojaresearcher <127495687+poojaresearcher@users.noreply.github.com>
2023-04-05 16:19:48 +0530

    Update anomaly detection.py

--- anomaly detection.py
+++ anomaly detection.py
@@ -481,6 +481,56 @@ if __name__ == '__main__':
             sys.exit(1)
 
 
+   # Create a Pandas dataframe from a Zeek log
+        try:
+            log_to_df = log_to_dataframe.LogToDataFrame()
+            zeek_df = log_to_df.create_dataframe(args.zeek_log)
+            print(zeek_df.head())
+        except IOError:
+            print('Could not open or parse the specified logfile: %s' % args.zeek_log)
+            sys.exit(1)
+        print('Read in {:d} Rows...'.format(len(zeek_df)))
+
+        # Using Pandas we can easily and efficiently compute additional data metrics
+        # Here we use the vectorized operations of Pandas/Numpy to compute query length
+        # We'll also compute entropy of the query
+        if log_type == 'dns':
+            zeek_df['query_length'] = zeek_df['query'].str.len()
+            zeek_df['answer_length'] = zeek_df['answers'].str.len()
+            zeek_df['entropy'] = zeek_df['query'].map(lambda x: entropy(x))
+
+        # Use the zat DataframeToMatrix class
+        to_matrix = dataframe_to_matrix.DataFrameToMatrix()
+        zeek_matrix = to_matrix.fit_transform(zeek_df[features])
+        print(zeek_matrix.shape)
+
+        # Train/fit and Predict anomalous instances using the Isolation Forest model
+        odd_clf = IsolationForest(contamination=0.2)  # Marking 20% as odd
+        odd_clf.fit(zeek_matrix)
+
+        # Now we create a new dataframe using the prediction from our classifier
+        predictions = odd_clf.predict(zeek_matrix)
+        odd_df = zeek_df[features][predictions == -1]
+        display_df = zeek_df[predictions == -1].copy()
+
+        # Now we're going to explore our odd observations with help from KMeans
+        odd_matrix = to_matrix.fit_transform(odd_df)
+        num_clusters = min(len(odd_df), 4)  # 4 clusters unless we have less than 4 observations
+        display_df['cluster'] = KMeans(n_clusters=num_clusters).fit_predict(odd_matrix)
+        print(odd_matrix.shape)
+
+        # Now group the dataframe by cluster
+        if log_type == 'dns':
+            features += ['query']
+        else:
+            features += ['host']
+        cluster_groups = display_df[features+['cluster']].groupby('cluster')
+
+        # Now print out the details for each cluster
+        print('<<< Outliers Detected! >>>')
+        for key, group in cluster_groups:
+            print('\nCluster {:d}: {:d} observations'.format(key, len(group)))
+            print(group.head())
 
 
 

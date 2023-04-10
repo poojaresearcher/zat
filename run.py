d52aa98c043ebd39724ce7cf9e853a23caeb3782
poojaresearcher <127495687+poojaresearcher@users.noreply.github.com>
2023-04-10 15:40:12 +0530

    Update run.py

--- run.py
+++ run.py
@@ -112,12 +112,10 @@ predictions = new_model.predict(zeek_matrix)
 zeek1_df = zeek_df[features][predictions == 1]
 display_df = zeek_df[predictions == 1].copy()
         
-if 
-    zeek_df[features][predictions == 0]
+if zeek_df[features][predictions == 0]:
     display_Df = zeek_df[predictions == 0].copy()
     print('legit domains')
-else:
-    zeek_df[features][predictions == 1]
+else zeek_df[features][predictions == 1]:
     display_Df = zeek_df[predictions == 1].copy()
     print('dga domains')
 

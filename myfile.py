68408f882f175aacbedb131f5d6aea33e0df9b51
poojaresearcher <127495687+poojaresearcher@users.noreply.github.com>
2023-04-13 15:36:35 +0530

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -163,7 +163,7 @@ for ngram, count in _sorted_ngrams[:10]:
     print = (ngram, count)
     
 def ngram_count(domain):    
-    domain_match = zeek_counts * zeek_vc.transform([domain]).T  # Woot vector multiply and transpose Woo Hoo!
+    domain_match = zeek_counts * zeek_vc.transform([domain].values.astype('U')).T  # Woot vector multiply and transpose Woo Hoo!
    
     print = ('%s domain match:%d') % (domain, domain_match)
     

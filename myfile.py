c71728603950259d32f87d6f7103861b90df97d6
poojaresearcher <127495687+poojaresearcher@users.noreply.github.com>
2023-04-17 17:09:13 +0530

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -88,6 +88,21 @@ if __name__ == '__main__':
             sys.exit(1)
         print('Read in {:d} Rows...'.format(len(zeek_df)))
 
+import time,datetime
+
+def sleep_till_future(f_minute):
+    """The function takes the current time, and calculates for how many seconds should sleep until a user provided minute in the future."""       
+    t = datetime.datetime.today()
+    future = datetime.datetime(t.year,t.month,t.day,t.hour,f_minute)
+    if future.minute <= t.minute:
+        print("ERROR! Enter a valid minute in the future.")
+    else:
+        print "Current time: " + str(t.hour)+":"+str(t.minute)
+        print "Sleep until : " + str(future.hour)+":"+str(future.minute)
+        seconds_till_future = (future-t).seconds
+        time.sleep( seconds_till_future )
+        print "I slept for "+str(seconds_till_future)+" seconds!"
+
 
 def entropy(string):
     """Compute entropy on the string"""

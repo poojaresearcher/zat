c26fcbc783b3426930cc450722909454f90ec762
poojaresearcher <127495687+poojaresearcher@users.noreply.github.com>
2023-04-17 17:55:14 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,98 +1,31 @@
+
 import os
 import sys
 import argparse
-import math
-from collections import Counter
-import pickle
-import re
-
-import numpy as np
-import pandas as pd
-from pandas import read_csv, concat
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, roc_auc_score 
-import sklearn.feature_extraction
-import matplotlib.pyplot as plt
-import seaborn as sns
-import re
-from sklearn import feature_extraction, tree, model_selection, metrics
-from yellowbrick.features import Rank2D
-from yellowbrick.features import RadViz
-from yellowbrick.features import ParallelCoordinates
-
-import sklearn.ensemble
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import cross_validate
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer
-
-import tldextract
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from keras.models import Sequential, load_model
-from keras.layers.core import Dense, Dropout, Activation,Embedding
-from keras.layers import LSTM
-
-import warnings
-warnings.filterwarnings('ignore')
-
-# Third Party Imports
-import pandas as pd
-
-from sklearn.cluster import KMeans
+from pprint import pprint
 
 # Local imports
-from zat import log_to_dataframe
-from zat import dataframe_to_matrix
-
-
-
+from zat import zeek_log_reader
 
 if __name__ == '__main__':
-    # Example to show the dataframe cache functionality on streaming data
-    pd.set_option('display.width', 1000)
+    # Example to run the zeek log reader on a given file
 
     # Collect args from the command line
     parser = argparse.ArgumentParser()
     parser.add_argument('zeek_log', type=str, help='Specify a zeek log to run ZeekLogReader test on')
+    parser.add_argument('-t', '--tail', action='store_true', help='Turn on log tailing')
     args, commands = parser.parse_known_args()
 
     # Check for unknown args
     if commands:
         print('Unrecognized args: %s' % commands)
-        sys.exit(1)                  
-  
+        sys.exit(1)
+
     # File may have a tilde in it
     if args.zeek_log:
         args.zeek_log = os.path.expanduser(args.zeek_log)
-  
-        # Sanity check either http or dns log
-        if 'http' in args.zeek_log:
-            log_type = 'http'
-            features = ['id.resp_p', 'method', 'resp_mime_types', 'request_body_len']
-        elif 'dns' in args.zeek_log:
-            log_type = 'dns'
-            features = ['Z','query', 'proto', 'qtype_name', 'query_length', 'answer_length', 'entropy']
-        else:
-            print('This example only works with Zeek with http.log or dns.log files..')
-            sys.exit(1)
-            
-def reading_log_files(filename):
-    with open(filename, "r") as f:
-        data = f.read().splitlines()
-    return data
 
-def log_generator(filename, period=1):
-    data = reading_log_files(filename)
-    while True:
-        time.sleep(period)
-        new_data = reading_log_files(filename)
-        yield new_data[len(data):]
-        data = new_data
-
-if __name__ == '__main__':
-    x = log_generator('home/logs/current/dns.log')
-    for lines in x:
-        print(lines)
+        # Run the zeek reader on a given log file
+        reader = zeek_log_reader.ZeekLogReader(args.zeek_log, tail=args.tail, strict=True)
+        for row in reader.readrows():
+            pprint(row)

ce79c3e16d692af74b339d28f3aedbb44a9a1e0e
poojaresearcher <127495687+poojaresearcher@users.noreply.github.com>
2023-04-17 15:26:58 +0530

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -156,7 +156,7 @@ model = tf.keras.models.load_model('dgadetection.h5')
 model.summary()
 
 X_test = zeek_df
-tf.convert_to_tensor(X_test, dtype=tf.str)
+tf.convert_to_tensor(X_test)
 a = (model.predict(X_test))
 print(a)
 

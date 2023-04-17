eb2a3b09cd5879f1fc6e0d2e1746701ab6b77dea
poojaresearcher <127495687+poojaresearcher@users.noreply.github.com>
2023-04-17 17:17:10 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,14 +1,96 @@
-import time,datetime
+import os
+import sys
+import argparse
+import math
+from collections import Counter
+import pickle
+import re
 
-def sleep_till_future(f_minute):
-    """The function takes the current time, and calculates for how many seconds should sleep until a user provided minute in the future."""       
-    t = datetime.datetime.today()
-    future = datetime.datetime(t.year,t.month,t.day,t.hour,f_minute)
-    if future.minute <= t.minute:
-        print("ERROR! Enter a valid minute in the future.")
-    else:
-        print "Current time: " + str(t.hour)+":"+str(t.minute)
-        print "Sleep until : " + str(future.hour)+":"+str(future.minute)
-        seconds_till_future = (future-t).seconds
-        time.sleep( seconds_till_future )
-        print "I slept for "+str(seconds_till_future)+" seconds!"
+import numpy as np
+import pandas as pd
+from pandas import read_csv, concat
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, roc_auc_score 
+import sklearn.feature_extraction
+import matplotlib.pyplot as plt
+import seaborn as sns
+import re
+from sklearn import feature_extraction, tree, model_selection, metrics
+from yellowbrick.features import Rank2D
+from yellowbrick.features import RadViz
+from yellowbrick.features import ParallelCoordinates
+
+import sklearn.ensemble
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_validate
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+
+import tldextract
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential, load_model
+from keras.layers.core import Dense, Dropout, Activation,Embedding
+from keras.layers import LSTM
+
+import warnings
+warnings.filterwarnings('ignore')
+
+# Third Party Imports
+import pandas as pd
+
+from sklearn.cluster import KMeans
+
+# Local imports
+from zat import log_to_dataframe
+from zat import dataframe_to_matrix
+
+
+
+
+if __name__ == '__main__':
+    # Example to show the dataframe cache functionality on streaming data
+    pd.set_option('display.width', 1000)
+
+    # Collect args from the command line
+    parser = argparse.ArgumentParser()
+    parser.add_argument('zeek_log', type=str, help='Specify a zeek log to run ZeekLogReader test on')
+    args, commands = parser.parse_known_args()
+
+    # Check for unknown args
+    if commands:
+        print('Unrecognized args: %s' % commands)
+        sys.exit(1)
+
+    # File may have a tilde in it
+    if args.zeek_log:
+        args.zeek_log = os.path.expanduser(args.zeek_log)
+
+        # Sanity check either http or dns log
+        if 'http' in args.zeek_log:
+            log_type = 'http'
+            features = ['id.resp_p', 'method', 'resp_mime_types', 'request_body_len']
+        elif 'dns' in args.zeek_log:
+            log_type = 'dns'
+            features = ['Z','query', 'proto', 'qtype_name', 'query_length', 'answer_length', 'entropy']
+        else:
+            print('This example only works with Zeek with http.log or dns.log files..')
+            sys.exit(1)
+            
+import schedule
+import time
+
+def read_key():
+    with open(zeek_log, 'r') as live_key_file_loc
+        live_token = live_key_file_loc.read()
+    print(live_token)
+
+schedule.every(30).minutes.do(read_key)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)            
+            
+            

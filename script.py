684dbc1ae0529d1ee3817e96b2f09a5d22b760c4
poojaresearcher <127495687+poojaresearcher@users.noreply.github.com>
2023-04-17 17:42:43 +0530

    Update script.py

--- script.py
+++ script.py
@@ -67,19 +67,14 @@ if __name__ == '__main__':
     # File may have a tilde in it
     if args.zeek_log:
         args.zeek_log = os.path.expanduser(args.zeek_log)
-        
-         f = open('zeek_log', 'r')
-while(1):
-    line = f.readline()
-    if line.find("ONE") != -1:
-        print "Got it"
-        
-f.write("One\n")
-f.close()    
-   
 
-
-
+        f = open('/tmp/workfile', 'r')
+        while(1):
+         line = f.readline()
+        if line.find("ONE") != -1:
+            print "Got it"
+         
+  
         # Sanity check either http or dns log
         if 'http' in args.zeek_log:
             log_type = 'http'

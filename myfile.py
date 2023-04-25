761c464b55705b97be37f7f57093b7fbd66bd3d2
poojaresearcher <127495687+poojaresearcher@users.noreply.github.com>
2023-04-25 17:06:36 +0530

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -74,19 +74,6 @@ def subdomain_extract(uri):
         return ext.subdomain 
 
 
-def vowel_consonant_ratio(x):
-    # Calculate vowel to consonant ratio
-    x = x.lower()
-    vowels_pattern = re.compile('([aeiou])')
-    consonants_pattern = re.compile('([b-df-hj-np-tv-z])')
-    vowels = re.findall(vowels_pattern, x)
-    consonants = re.findall(consonants_pattern, x)
-    try:
-        ratio = len(vowels) / len(consonants)
-    except: # catch zero devision exception 
-        ratio = 0  
-    return ratio
-
 def compute_ngrams(word_list, S=3, T=3):
     """Compute NGrams in the word_list from [S-T)
         Args:

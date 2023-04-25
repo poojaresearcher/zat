def5771ffa57fb955572489ffefd23c6e57f065f
poojaresearcher <127495687+poojaresearcher@users.noreply.github.com>
2023-04-25 16:13:11 +0530

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,31 +1,18 @@
-import pandas as pd
-import joblib
 from pyzeek import dns_log
 
-# Load your trained model
-model = joblib.load('dga_detection.pickle')
+# Define a function to handle DNS log data
+def handle_dns_log(data):
+    # Extract the relevant fields from the data
+    id_orig_h = data['id.orig_h']
+    id_resp_h = data['id.resp_h']
+    query = data['query']
 
-# Define a function to preprocess your data
-def preprocess_data(data):
-    # Extract relevant features from your data
-    features = ['id.orig_h', 'id.resp_h', 'query']
-    df = pd.DataFrame(data, columns=features)
+    # Process the data as needed (e.g. extract features, make predictions, etc.)
 
-    # Transform your data into a format that can be fed into your classifier
-    # For example, you could one-hot encode the 'query' feature
-    df = pd.get_dummies(df, columns=['query'])
-
-    return df
-
-# Define a function to make predictions using your model
-def predict(model, data):
-    # Preprocess your data
-    data = preprocess_data(data)
-
-    # Make predictions using your model
-    predictions = model.predict(data)
-
-    return predictions
+# Capture and parse DNS log data in real-time using PyZeek
+for log in dns_log('/path/to/your/zeek/logs', tail=True):
+    # Pass the DNS log data to your handling function
+    handle_dns_log(log)
 
 
 

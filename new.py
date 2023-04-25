a428e399bce17f680525c47b1781a678fd6528dd
poojaresearcher <127495687+poojaresearcher@users.noreply.github.com>
2023-04-25 16:00:13 +0530

    Update new.py

--- new.py
+++ new.py
@@ -131,7 +131,7 @@ def vowel_consonant_ratio(x):
         ratio = 0  
     return ratio
                        
-         if log_type == 'dns':
+if log_type == 'dns':
             zeek_df['query_length'] = zeek_df['query'].str.len()
             zeek_df['answer_length'] = zeek_df['answers'].str.len()
             zeek_df['entropy'] = zeek_df['query'].map(lambda x: entropy(x))

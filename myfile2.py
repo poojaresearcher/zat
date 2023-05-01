c786ad55b818d43c261047e49ee7a027bf37b82b
poojaresearcher <127495687+poojaresearcher@users.noreply.github.com>
2023-05-01 17:12:59 +0530

    Update myfile2.py

--- myfile2.py
+++ myfile2.py
@@ -181,7 +181,7 @@ def ngram_count(word, ngrams):
 if log_type == 'dns':
             zeek_df['query_length'] = zeek_df['query'].str.len()
             zeek_df['answer_length'] = zeek_df['answers'].str.len()
-            zeek_df['entropy'] = zeek_df['query'].applymap(lambda x: entropy(x))
+            zeek_df['entropy'] = zeek_df['query'].map(lambda x: entropy(x))
             zeek_df['vowel-cons'] = zeek_df['query'].map(lambda x: vowel_consonant_ratio(x))
             zeek_df['digits'] = zeek_df['query'].str.count('[0-9]')
             zeek_df['ngrams'] = zeek_df['query'].apply(ngrams)

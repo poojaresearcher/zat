206581a1857e01e7ec46fa95c4b1e3b0bfd77125
poojaresearcher <127495687+poojaresearcher@users.noreply.github.com>
2023-05-04 01:28:20 +0530

    Create read zeek log kafka.py

---
+++ read zeek log kafka.py
@@ -0,0 +1,34 @@
+import os
+from kafka import KafkaProducer
+
+# Kafka broker address
+bootstrap_servers = 'localhost:9092'
+
+# Kafka topic to produce messages
+topic = 'zeek_predictions'
+
+# Path to Zeek log files
+log_files_path = '/path/to/zeek/logs'
+
+# Create Kafka producer instance
+producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+
+# Read Zeek log files
+for file_name in os.listdir(log_files_path):
+    if file_name.endswith('.log'):
+        file_path = os.path.join(log_files_path, file_name)
+        with open(file_path, 'r') as file:
+            # Process each line of the log file
+            for line in file:
+                # Preprocess and extract relevant features from the log line
+                # Replace the following preprocessing and feature extraction steps with your own implementation
+                processed_data = preprocess_and_extract_features(line)
+
+                # Convert processed data to bytes
+                message = str(processed_data).encode('utf-8')
+
+                # Produce the message to the Kafka topic
+                producer.send(topic, message)
+
+# Close the Kafka producer
+producer.close()

e1dceefc5d258f965d3978ea09028d5ca0a89b00
poojaresearcher <127495687+poojaresearcher@users.noreply.github.com>
2023-05-12 16:05:58 +0530

    Update kafkaproducer.py

--- kafkaproducer.py
+++ kafkaproducer.py
@@ -32,7 +32,7 @@ for line in iter(zeek_proc.stdout.readline, b''):
     df['query'] = df['query'].str.split('.').str[::-1].str.join('.')
     df['query_length'] = df['query'].str.len()
     df['entropy'] = df['query'].map(lambda x: entropy(x))
-    df['digits'] = df.domain.str.count('[0-9]')
+    df['digits'] = df['query'].str.count('[0-9]')
     preprocessed_line = df.to_csv(header=False, index=False, sep='\t')
     print(df['digits'])
     

d466832944f78c2128033a3f28f00bd20347b276
poojaresearcher <127495687+poojaresearcher@users.noreply.github.com>
2023-05-12 17:39:05 +0530

    Update kafkaproducer.py

--- kafkaproducer.py
+++ kafkaproducer.py
@@ -50,6 +50,26 @@ def vowel_consonant_ratio (x):
     except: # catch zero devision exception 
         ratio = 0  
     return ratio
+   
+def compute_ngrams(word_list, S=3, T=3):
+    """Compute NGrams in the word_list from [S-T)
+        Args:
+            word_list (list): A list of words to compute ngram set from
+            S (int): The smallest NGram (default=3)
+            T (int): The biggest NGram (default=3)
+    """
+    _ngrams = []
+    if isinstance(word_list, str):
+        word_list = [word_list]
+    for word in word_list:
+        for n in range(S, T+1):
+            _ngrams += zip(*(word[i:] for i in range(n)))
+    return [''.join(_ngram) for _ngram in _ngrams]
+
+
+def ngram_count(word, ngrams):
+    """Compute the number of matching NGrams in the given word"""
+    return len(set(ngrams).intersection(compute_ngrams([word])))
 
 
 for line in iter(zeek_proc.stdout.readline, b''):
@@ -60,59 +80,9 @@ for line in iter(zeek_proc.stdout.readline, b''):
     df['entropy'] = df['query'].map(lambda x: entropy(x))
     df['digits'] = df['query'].str.count('[0-9]')
     df['vowel-cons'] = df['query'].map(lambda x: vowel_consonant_ratio(x))
-    
-    
-    
-    
-# Well our Mom told us we were still cool.. so with that encouragement we're
-# going to compute NGrams for every Alexa domain and see if we can use the
-# NGrams to help us better differentiate and mark DGA domains...
-
-# Scikit learn has a nice NGram generator that can generate either char NGrams or word NGrams (we're using char).
-# Parameters: 
-#       - ngram_range=(3,5)  # Give me all ngrams of length 3, 4, and 5
-#       - min_df=1e-4        # Minimumum document frequency. At 1e-4 we're saying give us NGrams that 
-#                            # happen in at least .1% of the domains (so for 100k... at least 100 domains)
-test_data_vc = sklearn.feature_extraction.text.CountVectorizer(analyzer='char', ngram_range=(3,5), min_df=1e-4, max_df=1.0)
-
-
-counts_matrix = test_data_vc.fit_transform(df['query'])
-td_counts = np.log10(counts_matrix.sum(axis=0).getA1())
-ngrams_list = test_data_vc.get_feature_names_out()
-
-import operator
-_sorted_ngrams = sorted(zip(ngrams_list, td_counts), key=operator.itemgetter(1), reverse=True)
-print = ('Alexa NGrams: %d') % len(_sorted_ngrams)
-for ngram, count in _sorted_ngrams[:10]:
-    print = (ngram, count)
-
-# We're also going to throw in a bunch of dictionary words
-word_dataframe = pd.read_csv('words.txt', names=['word'], header=None, dtype={'word': np.str}, encoding='utf-8')
-
-# Cleanup words from dictionary
-word_dataframe = word_dataframe[word_dataframe['word'].map(lambda x: str(x).isalpha())]
-word_dataframe = word_dataframe.applymap(lambda x: str(x).strip().lower())
-word_dataframe = word_dataframe.dropna()
-word_dataframe = word_dataframe.drop_duplicates()
-word_dataframe.head(10)
-
-# Now compute NGrams on the dictionary words
-# Same logic as above...
-dict_vc = sklearn.feature_extraction.text.CountVectorizer(analyzer='char', ngram_range=(3,5), min_df=1e-5, max_df=1.0)
-counts_matrix = dict_vc.fit_transform(word_dataframe['word'])
-dict_counts = np.log10(counts_matrix.sum(axis=0).getA1())
-ngrams_list = dict_vc.get_feature_names_out()
-
-import operator
-_sorted_ngrams = sorted(zip(ngrams_list, dict_counts), key=operator.itemgetter(1), reverse=True)
-print = ('Word NGrams: %d') % len(_sorted_ngrams)
-for ngram, count in _sorted_ngrams[:10]:
-    print = ('ngrams, count')
-
-    df['alexa_grams']= td_counts * test_data_vc.transform(df['query']).T 
-    df['word_grams']= dict_counts * dict_vc.transform(df['query']).T
-    df['diff'] = df['alexa_grams'] - df['word_grams']
-
+    df['ngrams'] = df['query'].map(lambda x: compute_ngrams(x))
+    df['ngram_count'] = df['query'].map(lambda x: ngram_count(x))
+   
     print(df.head(20))
   
     preprocessed_line = df.to_csv(header=False, index=False, sep='\t')

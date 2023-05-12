8689b1ec01471253821dd29c5047c699c331aff5
poojaresearcher <127495687+poojaresearcher@users.noreply.github.com>
2023-05-12 15:45:43 +0530

    Update kafkaproducer.py

--- kafkaproducer.py
+++ kafkaproducer.py
@@ -25,7 +25,7 @@ def entropy(string):
 for line in iter(zeek_proc.stdout.readline, b''):
     # Preprocess the DNS logs
     df = pd.read_csv(io.StringIO(line.decode('utf-8')), delimiter='\t', header=None)
-    df = df.drop(['ts', 'uid', 'id.orig_h', 'id.orig_p', 'id.resp_h', 'id.resp_p', 'proto', 'trans_id', 'qclass', 'qclass_name', 'qtype', 'qtype_name', 'rcode', 'rcode_name', 'AA', 'TC', 'RD', 'RA', 'Z', 'TTLs', 'rejected'], axis=1)
+    df = df.drop(['ts', 'uid', 'id.orig_h', 'id.orig_p', 'id.resp_h', 'id.resp_p', 'proto', 'trans_id', 'qclass', 'qclass_name', 'qtype', 'qtype_name', 'rcode', 'rcode_name', 'AA', 'TC', 'RD', 'RA', 'Z', 'TTLs', 'rejected'])
     df.columns = ['query','answers']
     df['query'] = df['query'].str.split('.').str[::-1].str.join('.')
     df['query_length'] = zeek_df['query'].str.len()

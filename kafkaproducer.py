151b003acaabbb609d8837fed2878f7aef8bc9a6
poojaresearcher <127495687+poojaresearcher@users.noreply.github.com>
2023-05-15 14:41:40 +0530

    Update kafkaproducer.py

--- kafkaproducer.py
+++ kafkaproducer.py
@@ -78,6 +78,7 @@ for line in iter(zeek_proc.stdout.readline, b''):
     df = pd.read_csv(io.StringIO(line.decode('utf-8')), delimiter='\t', header=None)
     df = pd.DataFrame([df])
     df = df.drop(['ts', 'uid', 'id.orig_h', 'id.orig_p', 'id.resp_h', 'id.resp_p', 'proto', 'trans_id', 'qclass', 'qclass_name', 'qtype', 'qtype_name', 'AA', 'TC', 'RD', 'RA', 'Z', 'rejected'], axis=1)
+    print(df)
     
     
    

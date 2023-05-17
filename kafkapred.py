7bcbeb4bb238148c69495bc0ef2545d836a0ed6f
poojaresearcher <127495687+poojaresearcher@users.noreply.github.com>
2023-05-17 14:54:28 +0530

    Create kafkapred.py

---
+++ kafkapred.py
@@ -0,0 +1,15 @@
+from kafka import KafkaProducer
+import time
+import subprocess
+from collections import Counter
+import math
+
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+zeek_proc = subprocess.Popen(['tail', '-f', '/opt/zeek/logs/current/dns.log'], stdout=subprocess.PIPE)
+
+for line in iter(zeek_proc.stdout.readline, b''):
+    producer.send('dns1', line.rstrip())
+    time.sleep(0.1)
+
+producer.close()
+

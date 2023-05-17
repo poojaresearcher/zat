38fefc5fcc0ac7a71e3f0ff768d098053e0b184c
poojaresearcher <127495687+poojaresearcher@users.noreply.github.com>
2023-05-17 14:30:21 +0530

    Update kafka1.py

--- kafka1.py
+++ kafka1.py
@@ -18,29 +18,30 @@ def entropy(string):
     return -sum(count/lns * math.log(count/lns, 2) for count in p.values())
 
 for line in iter(zeek_proc.stdout.readline, b''):
+    
 # Preprocessing function to extract features from query column
-def preprocess(df):
+    def preprocess(df):
     # Feature: entropy
-    df['domain'] = df['query'].str.split('.').str[::-1].str.join('.')
-    df['length'] = df['query'].str.len()
-    df['entropy'] = df['query'].map(lambda x: entropy(x))
-    df['digits'] = df['query'].str.count('[0-9]')
+        df['domain'] = df['query'].str.split('.').str[::-1].str.join('.')
+        df['length'] = df['query'].str.len()
+        df['entropy'] = df['query'].map(lambda x: entropy(x))
+        df['digits'] = df['query'].str.count('[0-9]')
    
     # Add more features as needed
-    return df
+        return df
 
 # Prediction function
-def predict(df):
-    X = df[['domain', 'length', 'entropy', 'digits']].to_numpy()  # Use relevant features for prediction
-    y_pred = model.predict(X)
-    return y_pred
+    def predict(df):
+        X = df[['domain', 'length', 'entropy', 'digits']].to_numpy()  # Use relevant features for prediction
+        y_pred = model.predict(X)
+        return y_pred
 
 # Stream dns.log data to Kafka topic
-for msg in consumer:
-    line = msg.value
-    df = pd.read_csv(io.StringIO(line.decode('utf-8')), delimiter='\t', header=None)
-    df.columns = ['ts', 'uid', 'id.orig_h', 'id.orig_p', 'id.resp_h', 'id.resp_p', 'proto', 'trans_id', 'query', 'qclass', 'qclass_name', 'qtype', 'qtype_name', 'rcode', 'rcode_name', 'AA', 'TC', 'RD', 'RA', 'Z', 'answers', 'TTLs', 'rejected']
-    df = preprocess(df)
-    y_pred = predict(df)
-    for p in y_pred:
-        producer.send('dns1', str(p).encode())
+    for msg in consumer:
+        line = msg.value
+        df = pd.read_csv(io.StringIO(line.decode('utf-8')), delimiter='\t', header=None)
+        df.columns = ['ts', 'uid', 'id.orig_h', 'id.orig_p', 'id.resp_h', 'id.resp_p', 'proto', 'trans_id', 'query', 'qclass', 'qclass_name', 'qtype', 'qtype_name', 'rcode', 'rcode_name', 'AA', 'TC', 'RD', 'RA', 'Z', 'answers', 'TTLs', 'rejected']
+        df = preprocess(df)
+        y_pred = predict(df)
+        for p in y_pred:
+            producer.send('dns1', str(p).encode())

a3c2bc388f469e16c79977273d335bfa6d5bc22d
poojaresearcher <127495687+poojaresearcher@users.noreply.github.com>
2023-05-29 14:26:18 +0530

    Update mykafkaconsumer.py

--- mykafkaconsumer.py
+++ mykafkaconsumer.py
@@ -11,7 +11,11 @@ consumer = KafkaConsumer('dnslogs', bootstrap_servers=['localhost:9092'],
      value_deserializer=lambda x: json.loads(x.decode('utf-8')))
 
 for message in consumer:
-    print(message.value)
+    dns_message = message.value
+
+     query = dns_message['query']
+     print('query')
+
 
 
 

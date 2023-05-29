176986f8e446eecdb58b72e61227e6b13d431aa9
poojaresearcher <127495687+poojaresearcher@users.noreply.github.com>
2023-05-29 16:48:12 +0530

    Update mykafkaconsumer.py

--- mykafkaconsumer.py
+++ mykafkaconsumer.py
@@ -7,6 +7,24 @@ import joblib
 import pickle
 import json
 
+def entropy(string):
+    """Compute entropy on the string"""
+    p, lns = Counter(string), float(len(string))
+    return -sum(count/lns * math.log(count/lns, 2) for count in p.values())
+  
+def vowel_consonant_ratio (x):
+    # Calculate vowel to consonant ratio
+    x = x.lower()
+    vowels_pattern = re.compile('([aeiou])')
+    consonants_pattern = re.compile('([b-df-hj-np-tv-z])')
+    vowels = re.findall(vowels_pattern, x)
+    consonants = re.findall(consonants_pattern, x)
+    try:
+        ratio = len(vowels) / len(consonants)
+    except: # catch zero devision exception 
+        ratio = 0  
+    return ratio
+  
 consumer = KafkaConsumer('dnslogs', bootstrap_servers=['localhost:9092'],
      value_deserializer=lambda x: json.loads(x.decode('utf-8')))
 
@@ -15,6 +33,9 @@ for message in consumer:
      query = dns_message['query']
      domain = dns_message['query'].str.split('.').str[::-1].str.join('.')
      print(domain)
+
+     
+ consumer.close()
      
     
 

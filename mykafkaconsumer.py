c9b496f6411c73d146c80ce4ea7e17faa11d2807
poojaresearcher <127495687+poojaresearcher@users.noreply.github.com>
2023-06-01 13:47:21 +0530

    Update mykafkaconsumer.py

--- mykafkaconsumer.py
+++ mykafkaconsumer.py
@@ -40,14 +40,10 @@ def extract_domain(query):
     subdomain = extracted.subdomain
     suffix = extracted.suffix
 
-    modified_query = query.replace(f".{suffix}", "")
-    print("Modified Query:", modified_query)
-
-    merged_domain = f"{subdomain}.{domain}.{suffix}"
-    print("Merged Domain:", merged_domain)
-
     domain_features['domain'] = domain
     domain_features['subdomain'] = subdomain
+    print(domain)
+    print(subdomain)
     
     return domain_features, modified_query
 
@@ -55,15 +51,17 @@ def extract_domain(query):
 def extract_features(query):
     features = {}
   
-    features['length'] = len(modified_query)
-    features['entropy'] = entropy(modified_query)
-    features['vowel_consonant_ratio'] = vowel_consonant_ratio(modified_query)
-    features['digits'] = sum(char.isdigit() for char in modified_query)
+    domain_features = {}
+    domain_features['length'] = len(domain)
+    domain_features['entropy'] = entropy(domain)
+    domain_features['vowel_consonant_ratio'] = vowel_consonant_ratio(domain)
+    domain_features['digits'] = sum(char.isdigit() for char in domain)
+    features['domain'] = domain_features
     
     alexa_vc = sklearn.feature_extraction.text.CountVectorizer(analyzer='char', ngram_range=(3, 5), min_df=1e-4, max_df=1.0)
 
 
-    alexa_counts_matrix = alexa_vc.fit_transform([modified_query])
+    alexa_counts_matrix = alexa_vc.fit_transform([domain])
     alexa_counts = np.log10(alexa_counts_matrix.sum(axis=0).A1)
     alexa_ngrams_list = alexa_vc.get_feature_names_out()
     
@@ -75,8 +73,6 @@ def extract_features(query):
     word_dataframe = word_dataframe.dropna()
     word_dataframe = word_dataframe.drop_duplicates()
     
-
-
     word_vc = sklearn.feature_extraction.text.CountVectorizer(analyzer='char', ngram_range=(3, 5), min_df=1e-5, max_df=1.0)
 
     word_counts_matrix = word_vc.fit_transform(word_dataframe['word'])
@@ -88,16 +84,60 @@ def extract_features(query):
         dict_match = word_counts * word_vc.transform([google]).T
         print(f'{google} Alexa match: {alexa_match}, Dict match: {dict_match}')
     
-    alexa_match = alexa_counts * alexa_vc.transform([modified_query]).T
+    alexa_match = alexa_counts * alexa_vc.transform([domain]).T
     features['alexa_grams'] = alexa_match.item() if alexa_match.size > 0 else 0
     
     # Compute word NGrams for the query
-    dict_match = word_counts * word_vc.transform([modified_query]).T
+    dict_match = word_counts * word_vc.transform([domain]).T
     features['word_grams'] = dict_match.item() if dict_match.size > 0 else 0
     
     # Compute the difference between Alexa NGrams and word NGrams
     features['diff'] = features['alexa_grams'] - features['word_grams']
+    features['domain'] = domain_features
+    
+    subdomain_features = {}
+    subdomain_features['length'] = len(subdomain)
+    subdomain_features['entropy'] = entropy(subdomain)
+    subdomain_features['vowel_consonant_ratio'] = vowel_consonant_ratio(subdomain)
+    subdomain_features['digits'] = sum(char.isdigit() for char in subdomain)
+    alexa_vc = sklearn.feature_extraction.text.CountVectorizer(analyzer='char', ngram_range=(3, 5), min_df=1e-4, max_df=1.0)
 
+
+    alexa_counts_matrix = alexa_vc.fit_transform([subdomain])
+    alexa_counts = np.log10(alexa_counts_matrix.sum(axis=0).A1)
+    alexa_ngrams_list = alexa_vc.get_feature_names_out()
+    
+    word_dataframe = pd.read_csv('words.txt', names=['word'], header=None, dtype={'word': str}, encoding='utf-8')
+
+# Cleanup words from dictionary
+    word_dataframe = word_dataframe[word_dataframe['word'].map(lambda x: str(x).isalpha())]
+    word_dataframe = word_dataframe.applymap(lambda x: str(x).strip().lower())
+    word_dataframe = word_dataframe.dropna()
+    word_dataframe = word_dataframe.drop_duplicates()
+    
+    word_vc = sklearn.feature_extraction.text.CountVectorizer(analyzer='char', ngram_range=(3, 5), min_df=1e-5, max_df=1.0)
+
+    word_counts_matrix = word_vc.fit_transform(word_dataframe['word'])
+    word_counts = np.log10(word_counts_matrix.sum(axis=0).A1)
+    word_ngrams_list = word_vc.get_feature_names_out()
+
+    def ngram_count(google):
+        alexa_match = alexa_counts * alexa_vc.transform([google]).T
+        dict_match = word_counts * word_vc.transform([google]).T
+        print(f'{google} Alexa match: {alexa_match}, Dict match: {dict_match}')
+    
+    alexa_match = alexa_counts * alexa_vc.transform([domain]).T
+    features['alexa_grams'] = alexa_match.item() if alexa_match.size > 0 else 0
+    
+    # Compute word NGrams for the query
+    dict_match = word_counts * word_vc.transform([domain]).T
+    features['word_grams'] = dict_match.item() if dict_match.size > 0 else 0
+    
+    # Compute the difference between Alexa NGrams and word NGrams
+    features['diff'] = features['alexa_grams'] - features['word_grams']
+    features['subdomain'] = subdomain_features
+    
+   
     return features
 
 classifier = joblib.load('dga_detection.joblib')
@@ -115,8 +155,8 @@ for message in consumer:
     print(query)
 
     # Preprocess and extract feature
-    domain_features, modified_query = extract_domain(query) 
-    features = extract_features(domain)
+    domain_features = extract_domain(query) 
+    features = extract_features(domain_features['domain'], domain_features['subdomain'])
     print(domain_features)
     print(features)
 
@@ -126,7 +166,7 @@ for message in consumer:
 
     # Prepare prediction output message
     prediction_message = {
-        'query': query,
+        'query': domain,subdomain
         'prediction': prediction
     }
 

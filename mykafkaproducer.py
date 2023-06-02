4962447dc9db71972538bc853547768e9d5a518a
poojaresearcher <127495687+poojaresearcher@users.noreply.github.com>
2023-06-02 15:16:27 +0530

    Update mykafkaproducer.py

--- mykafkaproducer.py
+++ mykafkaproducer.py
@@ -3,7 +3,7 @@ import time
 import subprocess
 
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
-zeek_proc = subprocess.Popen(['tail', '-f', '/opt/zeek/logs/current/dns.2023-06-02-14-01-33.log'], stdout=subprocess.PIPE)
+zeek_proc = subprocess.Popen(['tail', '-f', '/opt/zeek/logs/current/dns.log'], stdout=subprocess.PIPE)
 
 for line in iter(zeek_proc.stdout.readline, b''):
     producer.send('dnslogs', line.rstrip())

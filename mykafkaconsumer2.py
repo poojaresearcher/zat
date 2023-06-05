2e89d0ae28496edfdc871e5ca0fd3b41552772e0
poojaresearcher <127495687+poojaresearcher@users.noreply.github.com>
2023-06-05 17:47:32 +0530

    Update mykafkaconsumer2.py

--- mykafkaconsumer2.py
+++ mykafkaconsumer2.py
@@ -110,7 +110,7 @@ for message in consumer:
     domain_prediction = classifier.predict(domain_feature_array)[0]
     print("Domain Prediction:", domain_prediction)
     
-
+if domain_prediction == 'DGA':
     # Prepare prediction output message
     prediction_message = {
         'query': query,

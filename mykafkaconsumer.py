5e374d270c1811be86a6bd6832fe8c0bda7bd9c6
poojaresearcher <127495687+poojaresearcher@users.noreply.github.com>
2023-06-06 13:49:43 +0530

    Update mykafkaconsumer.py

--- mykafkaconsumer.py
+++ mykafkaconsumer.py
@@ -89,7 +89,8 @@ def extract_features(query):
 classifier = joblib.load('test_data/dga_detection3.joblib')
 
 consumer = KafkaConsumer('dnslogs', bootstrap_servers=['localhost:9092'],
-                         value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+                         value_deserializer=lambda x: json.loads(x.decode('utf-8')),
+                         enable_auto_commit=True)
 
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
 

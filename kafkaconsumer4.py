b13ab269107d8abf3d3a487973b0a8bc6fbc1f7f
poojaresearcher <127495687+poojaresearcher@users.noreply.github.com>
2023-06-30 17:22:59 +0530

    Update kafkaconsumer4.py

--- kafkaconsumer4.py
+++ kafkaconsumer4.py
@@ -1,56 +1,47 @@
 from kafka import KafkaConsumer, KafkaProducer
 import json
-import pandas as pd
 import numpy as np
 from keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
+# Load the trained model
 model = load_model('my_model.h5')
 
-consumer = KafkaConsumer('dns1', bootstrap_servers=['localhost:9092'],
+# Define Kafka consumer and producer
+consumer = KafkaConsumer('input_topic', bootstrap_servers='localhost:9092',
                          value_deserializer=lambda x: json.loads(x.decode('utf-8')))
 
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+producer = KafkaProducer(bootstrap_servers='localhost:9092',
                          value_serializer=lambda x: json.dumps(x).encode('utf-8'))
+
+# Kafka topics for input and output
+input_topic = 'dns1'
+output_topic = 'prediction'
 
 for message in consumer:
     dns_message = message.value
-    query = dns_message.get('query', 'default_value')
-    print("Query:", query)
+    domain = dns_message.get('domain', '')
+    print("Domain:", domain)
 
-    # Convert query to sequence
-    sequence = [c for c in query]
-
-    max_sequence_length_new = max(data['int_sequence'].apply(len))
-
-    vocabulary = set()
-    for seq in data['sequence']:
-      vocabulary.update(seq)
-
-    char_to_int = {c: i + 1 for i, c in enumerate(vocabulary)}
-
-    # Convert sequence to integer sequence
+    # Preprocess the domain
+    sequence = [c for c in domain]
     int_sequence = [char_to_int.get(c, 0) for c in sequence]
-
-    # Pad sequence to match the model's input length
     padded_sequence = pad_sequences([int_sequence], maxlen=max_sequence_length)
 
-    # Predict with the classifier model
-    domain_prediction = model.predict(padded_sequence)
-    print("Domain Prediction:", domain_prediction)
+    # Make prediction using the model
+    prediction = model.predict(padded_sequence)[0]
+    prediction_label = 'DGA' if prediction >= 0.5 else 'Legitimate'
+    print("Prediction:", prediction_label)
 
-    # Prepare prediction output message
+    # Prepare the prediction message
     prediction_message = {
-        'query': query,
-        'DGA domain prediction': 'DGA' if domain_prediction >= 0.5 else 'Legitimate'
+        'domain': domain,
+        'prediction': prediction_label
     }
 
-    # Publish prediction output to Kafka topic
-    producer.send(prediction, value=prediction_message)
+    # Publish the prediction message to output topic
+    producer.send(output_topic, value=prediction_message)
     producer.flush()
 
 consumer.close()
 producer.close()
-
-
-    

26e2f4a91d6a8ef9beb296b11b6ee8b5f5041f02
poojaresearcher <127495687+poojaresearcher@users.noreply.github.com>
2023-06-30 15:55:56 +0530

    Update kafkaproducer4.py

--- kafkaproducer4.py
+++ kafkaproducer4.py
@@ -1,1 +1,13 @@
+from kafka import KafkaProducer
+import time
+import subprocess
+
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+zeek_proc = subprocess.Popen(['tail', '-f', '/opt/zeek/logs/current/dns.log'], stdout=subprocess.PIPE)
+
+for line in iter(zeek_proc.stdout.readline, b''):
+    producer.send('dns1', line.rstrip())
+    time.sleep(0.1)
+
+producer.close()
 
